91f91cf12628d053a94b3f379fdd309fb16d1f80
/N3xus_Pr1m3#6007 <>
2021-05-21 12:50:31 -0300

    Auto Update Completo, segundo teste(sem o dist)

--- dist/database.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json');
-var db = low(adapter);
-db.defaults({ usersThatCreatedCalls: [], boostersThatCreatedCalls: [], suggestionsCache: [], reminders: [] }).write();
-exports.default = db;
